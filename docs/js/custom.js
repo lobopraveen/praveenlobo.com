3b0c9afbf8d0e37e373a2f318558b38e0095f403
travis <lobopraveen@gmail.com>
2018-04-28 00:23:05 +0000

    Travis deploy for 701b1ac2476aa754f161de408f1bf7d328e84b51. [skip ci]

---
+++ docs/js/custom.js
@@ -0,0 +1,9 @@
+var adsenseScript = document.createElement("script");
+adsenseScript.async = true;
+adsenseScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.head.appendChild(adsenseScript);
+
+(adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8315342424461780",
+    enable_page_level_ads: true
+});
